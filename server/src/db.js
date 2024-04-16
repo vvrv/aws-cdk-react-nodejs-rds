79f150ad5bd72a9fd2467164d0df1b5d3c8e86b9
Mason Wang <bigmasonwang@gmail.com>
2024-04-16 21:17:26 +0930

    setup DB and user controller

---
+++ server/src/db.js
@@ -0,0 +1,22 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: '127.0.0.1',
+    dialect: "mysql",
+  }
+);
+
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("Database synchronized");
+  })
+  .catch((error) => {
+    console.error("Failed to synchronize database:", error);
+  });
+
+module.exports = sequelize;
---
+++ server/src/index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const userRoutes = require("./routes/users");
+
+
+const app = express();
+app.use(helmet());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
